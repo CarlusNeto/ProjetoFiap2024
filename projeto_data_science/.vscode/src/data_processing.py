e79542af75075aa72aa6363817f9c96c557268ad
Carlos Neto <124171652+CarlusNeto@users.noreply.github.com>
2024-09-10 22:21:32 -0300

    Update data_processing.py

--- projeto_data_science/.vscode/src/data_processing.py
+++ projeto_data_science/.vscode/src/data_processing.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-def load_data(file_path):
+def load_data(dados_transcribe.csv):
     """
     Carrega os dados de um arquivo CSV.
     """
-    return pd.read_csv(file_path)
+    return pd.read_csv(dados_transcribe.csv)
 
 def preprocess_data(df):
     """
